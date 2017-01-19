b62bf5fade0c7d17e83245bf7fe866575ed55dbf
Joshua Clayton <joshua.clayton@gmail.com>
2017-01-19 06:00:19 -0500

    Switch to lenses for TodoList manipulation

--- src/TodoList.hs
+++ src/TodoList.hs
@@ -1,41 +1,53 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module TodoList
     ( Msg(..)
     , TodoList(..)
+
     , createNewTodoList
     , update
     ) where
 
-import           Data.Map.Strict ((!?))
+import           Control.Lens
 import qualified Data.Map.Strict as Map
 import           Data.Text (Text)
 import qualified Todo
 import           Todo hiding (Msg, update)
 
 data TodoList = TodoList
-    { tlTodos :: Map.Map TodoId Todo
-    , tlStartingTodoId :: TodoId
-    , tlTitle :: Text
+    { _tlTitle :: Text
+    , _tlTodos :: Map.Map TodoId Todo
+    , _tlStartingTodoId :: TodoId
     }
+
+makeLenses ''TodoList
 
 data Msg
     = UpdateTodo Todo.Msg TodoId
     | CreateTodo Text
     | DeleteTodo TodoId
+    | UpdateTitle Text
 
 createNewTodoList :: Text -> TodoList
-createNewTodoList = TodoList Map.empty (TodoId 0)
+createNewTodoList t = TodoList t Map.empty (TodoId 0)
 
 update :: Msg -> TodoList -> TodoList
 update (UpdateTodo msg id') l =
-    case originalTodo of
+    case findTodo l id' of
         Nothing -> l
-        Just t -> l { tlTodos = Map.adjust (const $ Todo.update msg t) id' todos }
-  where
-    todos = tlTodos l
-    originalTodo = todos !? id'
+        Just t -> l & tlTodos %~ Map.adjust (const $ Todo.update msg t) id'
 update (CreateTodo title) l =
-    l { tlTodos = Map.insert todoId newTodo (tlTodos l) }
+    l & tlTodos %~ uncurry Map.insert (generateNewTodo l title)
+update (UpdateTitle t) l =
+    l & tlTitle .~ t
+update (DeleteTodo id') l =
+    l & tlTodos %~ Map.delete id'
+
+findTodo :: TodoList -> TodoId -> Maybe Todo
+findTodo l id' = Map.lookup id' $ l ^. tlTodos
+
+generateNewTodo :: TodoList -> Text -> (TodoId, Todo)
+generateNewTodo l title = (nextId, newTodo)
   where
-    newTodo = Todo.createNewTodo todoId title
-    todoId = succ $ tlStartingTodoId l
-update (DeleteTodo tid) l = l { tlTodos = Map.delete tid (tlTodos l) }
+    newTodo = Todo.createNewTodo nextId title
+    nextId = succ $ l ^. tlStartingTodoId
