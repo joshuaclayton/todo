4b3816b442da14176d478ddff233dcbb37d41d9c
Joshua Clayton <joshua.clayton@gmail.com>
2017-01-20 05:00:09 -0500

    Add todo priority

--- src/Todo.hs
+++ src/Todo.hs
@@ -5,6 +5,7 @@ module Todo
     , Todo(..)
     , TodoId(..)
     , DueDate(..)
+    , TodoPriority(..)
 
     , update
     , createNewTodo
@@ -14,6 +15,7 @@ import Data.Text (Text)
 import Data.Time (UTCTime)
 
 data TodoStatus = Complete | Incomplete
+data TodoPriority = Low | Medium | High
 newtype DueDate = DueDate UTCTime
 newtype TodoId = TodoId Integer deriving (Enum, Eq, Ord)
 
@@ -22,6 +24,7 @@ data Todo = Todo
     , tTitle :: Text
     , tStatus :: TodoStatus
     , tDueDate :: Maybe DueDate
+    , tPriority :: TodoPriority
     }
 
 data Msg
@@ -29,12 +32,28 @@ data Msg
     | MarkIncomplete
     | UpdateDueDate DueDate
     | DeleteDueDate
+    | IncreasePriority
+    | DecreasePriority
+    | SetPriority TodoPriority
 
 createNewTodo :: TodoId -> Text -> Todo
-createNewTodo id' t = Todo id' t Incomplete Nothing
+createNewTodo id' t = Todo id' t Incomplete Nothing Low
 
 update :: Msg -> Todo -> Todo
 update MarkComplete t = t { tStatus = Complete }
 update MarkIncomplete t = t { tStatus = Incomplete }
 update (UpdateDueDate d) t = t { tDueDate = Just d }
 update DeleteDueDate t = t { tDueDate = Nothing }
+update IncreasePriority t = t { tPriority = increasePriority $ tPriority t }
+update DecreasePriority t = t { tPriority = decreasePriority $ tPriority t }
+update (SetPriority p) t = t { tPriority = p }
+
+increasePriority :: TodoPriority -> TodoPriority
+increasePriority Low = Medium
+increasePriority Medium = High
+increasePriority High = High
+
+decreasePriority :: TodoPriority -> TodoPriority
+decreasePriority Low = Low
+decreasePriority Medium = Low
+decreasePriority High = Medium
